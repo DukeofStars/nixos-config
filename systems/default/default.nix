475b415d5f2d4d4074cd889ded5a2d4be8fe8e32
Tristan Fox <foxtristan@proton.me>
2024-05-05 10:21:30 +1000

    Switch to nvidia offload (instead of sync, much better performance)

--- systems/default/default.nix
+++ systems/default/default.nix
@@ -127,10 +127,14 @@
       modesetting.enable = true;
       # For hybrid graphics
       prime = {
-        sync.enable = true;
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
       };
+      nvidiaSettings = true;
     };
   };
 
