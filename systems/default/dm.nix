58e22eb69055404bb156f0719949adc36d1c78a1
Tristan Fox <foxtristan@proton.me>
2024-07-22 11:29:41 +1000

    Change keyboard layout for startup hyprland

--- systems/default/dm.nix
+++ systems/default/dm.nix
@@ -2,7 +2,15 @@
 
 {
   config = {
-    environment.etc."greetd/hyprland.conf".text = "exec-once=${pkgs.greetd.regreet}/bin/regreet; hyprctl dispatch exit";
+    environment.etc."greetd/hyprland.conf".text = ''
+      exec-once=${pkgs.greetd.regreet}/bin/regreet; hyprctl dispatch exit
+      input = {
+          touchpad.natural_scroll = true;
+          kb_variant = "colemak";
+          accel_profile = "flat";
+          scroll_factor = 0.7;
+      }
+    '';
     services.greetd = {
       enable = true;
       settings.default_session = {
